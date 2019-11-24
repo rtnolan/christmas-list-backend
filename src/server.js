e0caa19a0846a8dfbbe16cafaea3f4ff7f5f0582
Tom Nolan <tomnolan95@gmail.com>
2019-11-24 17:28:50 +0000

    added items and lists plus tests

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import cors from 'cors'
 import { signup, signin, protect } from './utils/auth'
 import { connect } from './utils/db'
 import userRouter from './resources/user/user.router'
+import itemRouter from './resources/item/item.router'
+import listRouter from './resources/list/list.router'
 
 export const app = express()
 
@@ -20,6 +22,8 @@ app.post('/auth/signin', signin)
 
 app.use('/api', protect)
 app.use('/api/user', userRouter)
+app.use('/api/item', itemRouter)
+app.use('/api/list', listRouter)
 
 export const start = async () => {
   try {
